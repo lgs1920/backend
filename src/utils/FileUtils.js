545745ed131fef0dc07249e8cfb2d0012c519025
chdenat <christian.denat@orange.fr>
2024-07-11 08:39:24 +0200

    - files are sorted (reverse order by default)

--- src/utils/FileUtils.js
+++ src/utils/FileUtils.js
@@ -8,13 +8,16 @@ export class FileUtils {
      * @param {array} files
      * @return {Promise:array}
      */
-    static sortFilesByTime = async (directory,files) => {
+    static sortFilesByTime = async (directory,files,ascendant=false) => {
+        const sortFunction = ascendant
+        ?(a, b) => a.time - b.time
+        :(a, b) => b.time - a.time
         return files
             .map(fileName => ({
                 name: fileName,
                 time: fs.statSync(`${directory}/${fileName}`).mtime.getTime(),
             }))
-            .sort((a, b) => a.time - b.time)
+            .sort((a,b)=>ascendant)
             .map(file => ({
                     file:file.name,
                     time:file.time
