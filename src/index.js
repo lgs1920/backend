76f1e6743ae50a7c744600f0e3feed39c4d37f15
chdenat <christian.denat@orange.fr>
2024-09-18 17:47:31 +0200

    - add several parameters

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+
+/**********************************************************************************************************************
+ *                                                                                                                    *
+ * This file is part of the LGS1920/backend project.                                                                  *
+ *                                                                                                                    *
+ *                                                                                                                    *
+ * File: index.js                                                                                                     *
+ * Path: /home/christian/devs/assets/lgs1920/backend/src/index.js                                                     *
+ *                                                                                                                    *
+ * Author : Christian Denat                                                                                           *
+ * email: christian.denat@orange.fr                                                                                   *
+ *                                                                                                                    *
+ * Created on: 2024-09-18                                                                                             *
+ * Last modified: 2024-09-18                                                                                          *
+ *                                                                                                                    *
+ *                                                                                                                    *
+ * Copyright © 2024 LGS1920                                                                                           *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
+
 import cors                 from '@elysiajs/cors'
 import swagger              from '@elysiajs/swagger'
 import { Elysia }           from 'elysia'
-import configuration        from '../config.json'
-
 
 import version               from '../version.json'
 import { ChangelogResource } from './resources/ChangelogResource'
@@ -13,6 +31,12 @@ export const CHANGELOG_ROUTE = 'changelog'
 export const VERSIONS_ROUTE = 'versions'
 export const PING_ROUTE = 'ping'
 
+export const platforms = {
+    DEV:'development',
+    STAGING:'staging',
+    PROD:'production',
+    TEST:'test'
+}
 
 // Declares used resources
 const resources = new Map([
@@ -20,6 +44,14 @@ const resources = new Map([
                               [VERSIONS_ROUTE, new VersionsResource()],
                               [PING_ROUTE, new PingResource()],
                           ])
+
+// Get platform type and name
+var platform= {
+    type:process.env.PLATFORM_TYPE,
+    name:process.env.PLATFORM_NAME
+}
+
+// Launch backend app
 const app = new Elysia()
     /**
      * Swagger definition
@@ -27,7 +59,7 @@ const app = new Elysia()
     .use(swagger({
                      documentation: {
                          info:    {
-                             title:   configuration.server.name,
+                             title:   process.env.BACKEND_NAME,
                              version: version.api,
                          },
                          tags:    [
@@ -35,8 +67,8 @@ const app = new Elysia()
                          ],
                          servers: [
                              {
-                                 url:         `https://localhost:${configuration.server.port}`,
-                                 description: '',
+                                 url:         `${process.env.BACKEND_PROTOCOL}://localhost:${process.env.BACKEND_PORT}`,
+                                 description: process.env.BACKEND_NAME,
                              },
                          ],
                      },
@@ -57,8 +89,8 @@ const app = new Elysia()
     //.use(cors({origin: /^http(s)?:\/\/(?:localhost|localhost:5173|localhost:4173|studio\.lgs1920.fr)(?::\d+)?\/?$/}))
     .use(cors({origin: true}))
 
-    .listen(configuration.server.port)
+    .listen(process.env.BACKEND_PORT)
 
 console.log(
-  `LGS120 Backend (based on 🦊 Elysia) is running at ${app.server?.hostname}:${app.server?.port}`
+  `${process.env.BACKEND_NAME} is running at ${app.server?.hostname}:${app.server?.port}`
 );
